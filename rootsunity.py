97b58e1ec97081b8054e5270b07a437b5af590fc
ssilwa <silwal@mit.edu>
2017-01-05 14:47:18 -0500

    Update rootsunity.py

--- rootsunity.py
+++ rootsunity.py
@@ -1,17 +1,23 @@
-# Plots roots of random polynomials
+# Script that plots the roots of polynomials with ceofficeints uniformly chosen from a fixed interval.
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Specify degree and number of polynomials 
 poly = 50
 deg = 100
 roots = []
 realparts = []
 imagparts = []
 
+# Pick coefficients randomly from a [-1, 1]. This interval can easily be changed.
+# Calculate the roots using numpy's built in methods.
 for i in range(poly):
 	currpoly = np.random.uniform(-1, 1, deg)
 	roots.append(np.roots(currpoly))
+	
+
+# Plot the roots
 
 plt.style.use('bmh')
 unitcircle = plt.Circle((0, 0), 1, color='k', fill=False, ls = "--", linewidth = 2)
